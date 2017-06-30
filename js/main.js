dd2b682e8f6a83599f816d63a12d4dfbc4abc4b9
Parisa Shojaei <parisa.shojaei85@gmail.com>
2017-06-30 22:06:28 +0430

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+window.onscroll = function() {scrollFunction()};
+window.scroll({
+    behavior: 'smooth' 
+    });
 
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("return-to-top").style.display = "block";
+    } else {
+        document.getElementById("return-to-top").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Chrome, Safari and Opera 
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
